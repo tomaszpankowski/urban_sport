74a16a291e469e1ae6e7e901e4bbc87bfe7fa6b1
tomaszpankowski <--get-all>
2022-02-01 10:12:10 +0100

    routing modified

---
+++ src/app/sports/sports/sports.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sports',
+  templateUrl: './sports.component.html',
+  styleUrls: ['../../sass/styles.scss']
+})
+export class SportsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
